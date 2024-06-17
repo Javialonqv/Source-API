97c9a9f3f5b4aec14f4e427fcbe43d89285b01dc
Javier Alonso <101952739+Javialonqv@users.noreply.github.com>
2024-06-16 18:23:28 -0700

    Added template class file.

---
+++ API/GameEngine/GameEngineClassTemplateFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API;
+using API.GameEngine;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : MainBrain
+    {
+        // Start is called at the start of the runtime.
+        public override void Start()
+        {
+            
+        }
+
+        // Update is called every frame.
+        public override void Update()
+        {
+            
+        }
+    }
+}
