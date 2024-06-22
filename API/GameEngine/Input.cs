95e9f5375c9690ff1fecaab8bbe90f6efc8dd96f
Javier Alonso <101952739+Javialonqv@users.noreply.github.com>
2024-06-21 22:05:57 -0700

    New Input Logic.

--- API/GameEngine/Input.cs
+++ API/GameEngine/Input.cs
@@ -8,47 +8,32 @@ namespace API.GameEngine
 {
     public static class Input
     {
-        static ConsoleKeyInfo currentKey;
-        static ConsoleKeyInfo previousKey;
-        static bool isKeyPressed = false;
-        static bool isKeyDown = false;
-        static bool isKeyUp = false;
+        static HashSet<ConsoleKey> currentKeys = new HashSet<ConsoleKey>();
+        static HashSet<ConsoleKey> previousKeys = new HashSet<ConsoleKey>();
 
         internal static void Update()
         {
-            bool avaiable = false;
-            if ((avaiable = Console.KeyAvailable))
-            {
-                currentKey = Console.ReadKey(true);
+            previousKeys = new HashSet<ConsoleKey>(currentKeys);
+            currentKeys.Clear();
 
-                isKeyPressed = true;
-                isKeyDown = currentKey.Key != previousKey.Key;
-                isKeyUp = false;
-
-                previousKey = currentKey;
-            }
-            else
+            while (Console.KeyAvailable)
             {
-                isKeyPressed = false;
-                isKeyDown = false;
-                isKeyUp = previousKey.Key != 0;
-                currentKey = new ConsoleKeyInfo();
-                previousKey = new ConsoleKeyInfo();
+                var key = Console.ReadKey(true).Key;
+                currentKeys.Add(key);
             }
-            Debug.Log("Current: " + currentKey.Key + " Previous one: " + previousKey.Key + " Key Avaiable: " + avaiable);
         }
 
-        public static bool GetKeyDown(ConsoleKey consoleKey)
-        {
-            return currentKey.Key == consoleKey && isKeyDown;
-        }
         public static bool GetKey(ConsoleKey consoleKey)
         {
-            return currentKey.Key == consoleKey && isKeyPressed;
+            return currentKeys.Contains(consoleKey);
+        }
+        public static bool GetKeyDown(ConsoleKey consoleKey)
+        {
+            return currentKeys.Contains(consoleKey) && !previousKeys.Contains(consoleKey);
         }
         public static bool GetKeyUp(ConsoleKey consoleKey)
         {
-            return currentKey.Key == consoleKey && isKeyUp;
+            return !currentKeys.Contains(consoleKey) && previousKeys.Contains(consoleKey);
         }
     }
 }
