ae9d5d6c4c21d6c04b994dd0431b033bf67f9eac
Javier Alonso <101952739+Javialonqv@users.noreply.github.com>
2024-07-10 16:40:19 -0700

    Added comments to Input class.

--- API/GameEngine/Input.cs
+++ API/GameEngine/Input.cs
@@ -7,33 +7,66 @@ using System.Threading.Tasks;
 
 namespace API.GameEngine
 {
+    /// <summary>
+    /// Main class for the managment of the Input system.
+    /// </summary>
     public static class Input
     {
+        /// <summary>
+        /// Specifies the current pressed keys on this frame.
+        /// </summary>
         static HashSet<ConsoleKey> currentKeys = new HashSet<ConsoleKey>();
+        /// <summary>
+        /// Specifies the previous pressed keys on the previous frame.
+        /// </summary>
         static HashSet<ConsoleKey> previousKeys = new HashSet<ConsoleKey>();
 
+        /// <summary>
+        /// Used for get the current state of the specified key using the user32.dll.
+        /// </summary>
+        /// <param name="vKey"></param>
+        /// <returns></returns>
         [DllImport("user32.dll")]
         static extern short GetAsyncKeyState(int vKey);
 
+        /// <summary>
+        /// This method is called every frame by an external script.
+        /// </summary>
         internal static void Update()
         {
             previousKeys = new HashSet<ConsoleKey>(currentKeys);
             currentKeys.Clear();
 
+            // Check for all the keys and check if they are pressed:
             for (int i = 0; i < 256; i++)
             {
                 if (GetAsyncKeyState(i) < 0) { currentKeys.Add((ConsoleKey)i); }
             }
         }
 
+        /// <summary>
+        /// Returns true while the specified key is pressed.
+        /// </summary>
+        /// <param name="consoleKey">The key to check if pressed.</param>
+        /// <returns></returns>
         public static bool GetKey(ConsoleKey consoleKey)
         {
             return currentKeys.Contains(consoleKey);
         }
+        /// <summary>
+        /// Returns true during the frame the user is pressing the specified key.
+        /// </summary>
+        /// <param name="consoleKey">The key to check if pressed.</param>
+        /// <returns></returns>
         public static bool GetKeyDown(ConsoleKey consoleKey)
         {
             return currentKeys.Contains(consoleKey) && !previousKeys.Contains(consoleKey);
         }
+        /// <summary>
+        /// Returns true during the frame the user is releasing the specified key.
+        /// </summary>
+        /// <param name="consoleKey">The key to check if released.</param>
+        /// <returns></returns>
         public static bool GetKeyUp(ConsoleKey consoleKey)
         {
             return !currentKeys.Contains(consoleKey) && previousKeys.Contains(consoleKey);
